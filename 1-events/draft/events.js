693ec6d56920116b2dabd8015407b9793bd8a685
Dave Wheeler <dwheelerau@gmail.com>
2020-02-19 11:28:24 +1100

    added files

---
+++ 1-events/draft/events.js
@@ -0,0 +1,34 @@
+async function createEvent() {
+  const rectColors = [
+    "yellowgreen",
+    "cornflowerblue",
+    "seagreen",
+    "slateblue",
+  ]
+
+  // create and bind data to our rects
+  const rects = d3.select("#svg")
+    .selectAll(".rect")
+    .data(rectColors)
+    .enter().append("rect")
+      .attr("height", 100)
+      .attr("width", 100)
+      .attr("x", (d,i) => i * 110)
+      .attr("fill", "lightgrey")
+
+    // cant use => fat arrow function notation with "this"
+    // We use this to target the element that was hovered over
+  rects.on("mouseenter", function(datum, index, nodes) {
+      d3.select(this).style("fill", datum)
+  })
+        .on("mouseout", function() {
+      d3.select(this).style("fill", "lightgrey")
+  })
+  setTimeout(() => {
+      rects
+          .dispatch("mouseout")
+          .on("mouseenter", null)
+          .on("mouseout", null)
+  }, 3000)
+}
+createEvent()
